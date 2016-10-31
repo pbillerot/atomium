2e600061fc3f3aba7781fbfb36a2fc57068dd6cf
pbillerot <philippe.billerot@gmail.com>
2016-10-31 18:44:27 +0100

    Avant abandon material-ui de Google

--- app.jsx
+++ app.jsx
@@ -4,25 +4,69 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-//import FlatButton from 'material-ui/Flat-button/index'
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import RaisedButton from 'material-ui/RaisedButton';
+import Toggle from 'material-ui/Toggle';
 
 injectTapEventPlugin()
 
-const App = () => (
- <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <RaisedButtonExampleSimple />
- </MuiThemeProvider>
-)
-
 export default class Main extends React.Component {
   render() {
-    //return <div>Hello from React with ES6 :)</div>;
     return <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <RaisedButtonExampleSimple />
+      <AppBarExampleComposition />
     </MuiThemeProvider>
   }
 }
+
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+class AppBarExampleComposition extends Component {
+  state = {
+    logged: true,
+  };
+
+  handleChange = (event, logged) => {
+    this.setState({logged: logged});
+  };
+
+  render() {
+    return (
+      <div>
+        <Toggle
+          label="Logged"
+          defaultToggled={true}
+          onToggle={this.handleChange}
+          labelPosition="right"
+          style={{margin: 20}}
+        />
+        <AppBar
+          title="Title"
+          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+          iconElementRight={this.state.logged ? <Logged /> : <Login />}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
 
 class H1 extends React.Component {
   render() {
