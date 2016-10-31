8dd08830c96d9b97d328f63f47bf43f89e61dc89
pbillerot <philippe.billerot@gmail.com>
2016-10-31 18:53:05 +0100

    Sans material-iu de Google

--- app.jsx
+++ app.jsx
@@ -1,71 +1,12 @@
 'use babel';
 import React from 'react'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import RaisedButton from 'material-ui/RaisedButton';
-import Toggle from 'material-ui/Toggle';
-
-injectTapEventPlugin()
 
 export default class Main extends React.Component {
   render() {
-    return <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <AppBarExampleComposition />
-    </MuiThemeProvider>
+    return <H1 />
   }
 }
 
-
-/**
- * This example is taking advantage of the composability of the `AppBar`
- * to render different components depending on the application state.
- */
-class AppBarExampleComposition extends Component {
-  state = {
-    logged: true,
-  };
-
-  handleChange = (event, logged) => {
-    this.setState({logged: logged});
-  };
-
-  render() {
-    return (
-      <div>
-        <Toggle
-          label="Logged"
-          defaultToggled={true}
-          onToggle={this.handleChange}
-          labelPosition="right"
-          style={{margin: 20}}
-        />
-        <AppBar
-          title="Title"
-          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-          iconElementRight={this.state.logged ? <Logged /> : <Login />}
-        />
-      </div>
-    );
-  }
-}
-
-
-
-const AppBarExampleIcon = () => (
-  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-);
 
 
 class H1 extends React.Component {
@@ -73,19 +14,3 @@ class H1 extends React.Component {
     return <h1>Hello from React with ES6 :)</h1>;
   }
 }
-
-const style = {
-  margin: 5,
-};
-
-const RaisedButtonExampleSimple = () => (
-  <div>
-    <RaisedButton label="Default" style={style} />
-    <RaisedButton label="Primary" primary={true} style={style} />
-    <RaisedButton label="Secondary" secondary={true} style={style} />
-    <RaisedButton label="Disabled" disabled={true} style={style} />
-    <br />
-    <br />
-    <RaisedButton label="Full width" fullWidth={true} />
-  </div>
-);
