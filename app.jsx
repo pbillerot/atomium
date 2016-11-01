d1b3f65b95f0927b4d1777f8eee533b2223af772
pbillerot <philippe.billerot@gmail.com>
2016-11-01 19:57:19 +0100

    Avec react-bbotstrap -> abandon

--- app.jsx
+++ app.jsx
@@ -1,91 +1,49 @@
 'use babel';
 import React from 'react'
-import Appbar from 'muicss/lib/react/appbar'
-import Button from 'muicss/lib/react/button'
-import Container from 'muicss/lib/react/container'
-import Dropdown from 'muicss/lib/react/dropdown';
-import DropdownItem from 'muicss/lib/react/dropdown-item';
-import Panel from 'muicss/lib/react/panel';
+
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 
 export default class Main extends React.Component {
   render() {
     return (
       <div>
-      <ContainerExample />
+      <Buttons />
       </div>
     )
   }
 }
 
-class ContainerExample extends React.Component {
+
+class Buttons extends React.Component {
   onClick() {
-    console.log('clicked on button');
+    console.log(`click: `);
   }
-  
   render() {
     return (
-      <div>
-        <AppbarExample />
-        <Container>
-          <Panel>
-            <Button onClick={this.onClick}>My Button</Button>
-          </Panel>
-        </Container>
-      </div>
-    );
-  }
-}
+        <ButtonToolbar>
+    {/* Standard button */}
+    <Button onClick={this.onClick}>Default</Button>
 
-class AppbarExample extends React.Component {
-  render() {
-    let s1 = {verticalAlign: 'middle'};
-    let s2 = {textAlign: 'right'};
+    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+    <Button bsStyle="primary">Primary</Button>
 
-    return (
-      <Appbar>
-       <table width="100%">
-         <tbody>
-           <tr style={s1}>
-             <td className="mui--appbar-height">Left Side</td>
-             <td className="mui--appbar-height" style={s2}>Right Side</td>
-           </tr>
-         </tbody>
-       </table>
-      </Appbar>
-    );
-  }
-}
+    {/* Indicates a successful or positive action */}
+    <Button bsStyle="success">Success</Button>
 
-class DropdownExample extends React.Component {
-  render() {
-    return (
-      <Dropdown color="primary" label="Dropdown">
-        <DropdownItem link="#/link1">Option 1</DropdownItem>
-        <DropdownItem>Option 2</DropdownItem>
-        <DropdownItem>Option 3</DropdownItem>
-        <DropdownItem>Option 4</DropdownItem>
-      </Dropdown>
-    );
-  }
-}
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
 
-class LigneExample extends React.Component {
-  render() {
-    let style = {height: '10px'};
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
 
-    return (
-      <div>
-        <div className="mui--bg-primary" style={style}></div>
-        <div className="mui--bg-primary-dark" style={style}></div>
-        <div className="mui--bg-primary-light" style={style}></div>
-        <br />
-        <div className="mui--bg-accent" style={style}></div>
-        <div className="mui--bg-accent-dark" style={style}></div>
-        <div className="mui--bg-accent-light" style={style}></div>
-        <br />
-        <div className="mui--bg-danger" style={style}></div>
-      </div>
-    );
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>      
+    )
   }
 }
 
