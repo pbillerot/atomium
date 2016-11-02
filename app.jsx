0b21c6648236fdb0ee7c9228ad2c4b47a876942f
pbillerot <philippe.billerot@gmail.com>
2016-11-02 16:08:09 +0100

    React ES6 Photon

--- app.jsx
+++ app.jsx
@@ -1,16 +1,58 @@
 'use babel';
 
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 export default class Main extends React.Component {
   render() {
-    return <div>Hello from React with ES6 :)</div>;
+      var myStyle = {
+         fontSize: 100,
+         color: '#FF0000'
+      }
+
+      return (
+        <div className="window">
+          <Header />
+          <Content />
+          <Footer />
+        </div>
+      );
   }
 }
 
-
-class H1 extends React.Component {
-  render() {
-    return '<h1>Hello from React with ES6 :)</h1>'
-  }
+class Header extends React.Component {
+   render() {
+      return (
+         <header className="toolbar toolbar-header">
+            <h1 className="title">HEADER</h1>
+         </header>
+      );
+   }
 }
+
+class Footer extends React.Component {
+   render() {
+      return (
+         <footer className="toolbar toolbar-footer">
+            <div className="title">
+            <a href="https://github.com/pbillerot/atomium" target="_blank">Github</a>
+            </div>
+         </footer>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+        <div className="window-content">
+          <div className="pane-group">
+            <div className="pane-sm sidebar">SIDEBAR</div>
+            <div className="pane">CONTENT</div>
+          </div>
+        </div>
+      );
+   }
+}
+
+
