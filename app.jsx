158428fe973c9110e53578d2aedd734238add8b1
pbillerot <philippe.billerot@gmail.com>
2016-11-02 10:19:38 +0100

    React ES6 photon

--- app.jsx
+++ app.jsx
@@ -1,54 +1,16 @@
 'use babel';
-import React from 'react'
 
-import Button from 'react-bootstrap/lib/Button';
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import React from 'react';
 
 export default class Main extends React.Component {
   render() {
-    return (
-      <div>
-      <Buttons />
-      </div>
-    )
+    return <div>Hello from React with ES6 :)</div>;
   }
 }
 
-
-class Buttons extends React.Component {
-  onClick() {
-    console.log(`click: `);
-  }
-  render() {
-    return (
-        <ButtonToolbar>
-    {/* Standard button */}
-    <Button onClick={this.onClick}>Default</Button>
-
-    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-    <Button bsStyle="primary">Primary</Button>
-
-    {/* Indicates a successful or positive action */}
-    <Button bsStyle="success">Success</Button>
-
-    {/* Contextual button for informational alert messages */}
-    <Button bsStyle="info">Info</Button>
-
-    {/* Indicates caution should be taken with this action */}
-    <Button bsStyle="warning">Warning</Button>
-
-    {/* Indicates a dangerous or potentially negative action */}
-    <Button bsStyle="danger">Danger</Button>
-
-    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-    <Button bsStyle="link">Link</Button>
-  </ButtonToolbar>      
-    )
-  }
-}
 
 class H1 extends React.Component {
   render() {
-    return <h1>Hello from React with ES6 :)</h1>;
+    return '<h1>Hello from React with ES6 :)</h1>'
   }
 }
